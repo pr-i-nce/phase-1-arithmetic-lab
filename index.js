630809057171d7d9ff5c41d49e2c5bc7b397fe1f
prince mumo <princeinkunzi@gmail.com>
2024-03-20 20:33:52 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+function generateRandomInteger() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+const random = generateRandomInteger();
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+const numbers = [5, 10, 20];
+const max = Math.max(...numbers);
+
+
